54182540f96bc2f7c61d3eeccb968635ea74e045
gracevthlia <114101163+gracevthlia@users.noreply.github.com>
2022-12-13 13:31:07 -0300

    Add files via upload

---
+++ thenews1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>the news| as notícias que você precisa para se manter informado</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="shortcut icon" href="30aa25285cee1438aede56326eda3eb9-icone-de-traco-colorido-da-xicara-de-cafe.png">
+    
+</head>
+<body>
+
+    <div class= "top-page">
+      <img src="LOGO_OFICIAL_thenews-1.png" class="top-page-image" alt="index">
+      <button class="button-as-link">
+        <a href="pagina2.php" target="_blank">
+          Assine aqui!
+        </a>
+      </button>
+
+      </div>
+        <div class="container">
+          <input type="checkbox" id="checkbox-menu">
+          <label for="checkbox-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+        </div>
+        <div class="right-page">
+            <div class="rightcard-page">
+             <!-- <h1>Mais Populares</h1>-->
+            </div>
+          </div>
+          <div class="left-page">
+              <div class="leftcard-page">
+                <!-- <h1>Últimas notícias</h1>-->
+              </div>
+    <div class="main-page">
+        <div class="cardmain-page">
+          <div class="card-noticia1">
+
+          </div>
+              </div>
+        </div>
+    </div>
+    <div class="down-page">
+    <h1>footer</h1> 
+    <!--  vou ter que criar outras divs para colocar o conteudo do footer, também preciso lembrar de diminuir o tamanho do footer 
+      porque ele ta enorme, ver como que adiciona notícias -->
+    </div>
+</body>
+</html>
